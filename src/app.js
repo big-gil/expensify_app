bd85100d0da48a52eca1a7d365656f1d6a23fd45
Gil <baxter@exis.net>
2020-09-28 11:38:23 -0400

    Removed hard coded dispath statements

--- src/app.js
+++ src/app.js
@@ -20,41 +20,8 @@ import moment from "moment";
 
 const store = configureStore();
 
-store.dispatch(
-  addExpense({
-    description: "Water Bill",
-    amount: 7200,
-    createDate: moment().add(7, "days").valueOf()
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Gas Bill",
-    amount: 10400,
-    createDate: moment().add(1, "days").valueOf()
-  })
-);
-store.dispatch(
-  addExpense({
-    description: "Rent",
-    amount: 600000,
-    createDate: moment().add(4, "days").valueOf()
-  })
-);
-
-// store.subscribe(() => {
 const state = store.getState();
-console.log(state);
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-// });
-
-// store.dispatch(setTextFilter("gas"));
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter("bill"));
-// }, 3000);
 
 const Jsx = (
   <Provider store={store}>
